3705cd333a0d5419ad112eecfab45f2cdf161ccf
Gihan Madushanka <madushanka@alliontechnologies.com>
2017-05-31 12:03:59 +0530

    Added onchangrhandler

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-#Adding text input and button to our child component
-#but We cannot type anything on the text input
+#onChangeHandler
--- src/app/components/Task.js
+++ src/app/components/Task.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 export class Task extends React.Component{
+    constructor(props){
+        super();
+        this.state = {
+            'input' : props.name
+        }
+
+    }
+
+    onChangeHandler(event){
+      this.setState({
+          'input' : event.target.value
+      })
+        
+    }
+    
     render(){
         return(
             <div className="row">
                 <div className="col-sm-6 col-sm-offset-4">
-                    <input type="text" value={this.props.name} placeholder="Type your task here" className="col-sm-4"/>
+                    <input type="text" value={this.state.input} placeholder="Type your task here" className="col-sm-4"
+                    onChange={(event)=>this.onChangeHandler(event)}
+                    />
                     <button className="btn-sm btn-primary">Add</button>
                 </div>         
                 
